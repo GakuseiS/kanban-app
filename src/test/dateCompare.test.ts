16d6435a6f7c83d10fcda25e90d39a6aa177803d
Ilya Spiridonov <i.spiridonov@l-tech.ru>
2025-01-12 23:57:53 +0300

    refactor code

--- src/test/dateCompare.test.ts
+++ src/test/dateCompare.test.ts
@@ -1,11 +1,11 @@
 import { isDateInPast } from '../utils/dateCompare';
 
 describe('isDateInPast', () => {
-  test('checks if 01.01.2000 in past', () => {
+  test('checks if 01.01.2000 is in past', () => {
     const dateInPast = new Date(2000, 1, 1);
     expect(isDateInPast(dateInPast)).toBe(true);
   });
-  test('checks if 01.01.3000 in past', () => {
+  test('checks if 01.01.3000 is in past', () => {
     const dateInFuture = new Date(3000, 1, 1);
     expect(isDateInPast(dateInFuture)).toBe(false);
   });
